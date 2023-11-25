9ebca5a3c075c677b154cf6e7d287b1406c0a7b7
Owen Bean <owenbean400@gmail.com>
2023-11-25 00:37:46 -0500

    Refactor cooking steps and ingredient view for 0, 1, or many ingredients/steps

--- lib/view/window/recipe/widgets/cooking_steps_view.dart
+++ lib/view/window/recipe/widgets/cooking_steps_view.dart
@@ -4,39 +4,51 @@ import 'package:recipe_roots/domain/cooking_step.dart';
 import 'package:recipe_roots/helper/map_index.dart';
 import 'package:recipe_roots/view/window/recipe/widgets/cooking_step_view.dart';
 
-class CookingStepsView extends StatefulWidget {
+class CookingStepsView extends StatelessWidget {
   final List<CookingStep> cookingSteps;
-
-  const CookingStepsView({Key? key, required this.cookingSteps})
-      : super(key: key);
-
-  @override
-  CookingStepsViewState createState() => CookingStepsViewState();
-}
-
-class CookingStepsViewState extends State<CookingStepsView> {
   final double borderRadius = 2;
+
+  const CookingStepsView({required this.cookingSteps, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-      items: mapIndexed(widget.cookingSteps, (index, item) {
-        return Builder(
-          builder: (BuildContext context) {
-            return Container(
-                width: MediaQuery.of(context).size.width,
-                margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                decoration: BoxDecoration(
-                    color: Theme.of(context).hintColor,
-                    borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(borderRadius),
-                        bottomRight: Radius.circular(borderRadius))),
-                child: CookingStepView(
-                    instruction: item.instruction, order: index));
-          },
-        );
-      }).toList(),
-      options: CarouselOptions(height: 400.0),
-    );
+    return (cookingSteps.isNotEmpty)
+        ? (cookingSteps.length == 1)
+            ? Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                      height: 400,
+                      width: MediaQuery.of(context).size.width,
+                      margin: const EdgeInsets.symmetric(horizontal: 36),
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).hintColor,
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(borderRadius),
+                              bottomRight: Radius.circular(borderRadius))),
+                      child: CookingStepView(
+                          instruction: cookingSteps.first.instruction,
+                          order: 0));
+                },
+              )
+            : CarouselSlider(
+                items: mapIndexed(cookingSteps, (index, item) {
+                  return Builder(
+                    builder: (BuildContext context) {
+                      return Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: BoxDecoration(
+                              color: Theme.of(context).hintColor,
+                              borderRadius: BorderRadius.only(
+                                  bottomLeft: Radius.circular(borderRadius),
+                                  bottomRight: Radius.circular(borderRadius))),
+                          child: CookingStepView(
+                              instruction: item.instruction, order: index));
+                    },
+                  );
+                }).toList(),
+                options: CarouselOptions(height: 400.0),
+              )
+        : Container();
   }
 }

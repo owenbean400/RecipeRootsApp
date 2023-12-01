914c52f15a0e6aae0ee3fb299b9717ae63a50183
Shea Durgin <shea.durgin@maine.edu>
2023-11-30 23:32:27 -0500

    Save node positions

--- lib/view/window/family_tree/family_tree_view.dart
+++ lib/view/window/family_tree/family_tree_view.dart
@@ -5,6 +5,7 @@ import 'package:recipe_roots/domain/person.dart';
 import 'package:recipe_roots/helper/the_person.dart';
 import 'package:recipe_roots/service/family_service.dart';
 import 'package:recipe_roots/view/widget/header_person_list.dart';
+import 'package:recipe_roots/service/node_position_service.dart';
 
 class FamilyTreeView extends StatefulWidget {
   final ValueSetter<ChildToParent?> setAddFamilyTree;
@@ -20,29 +21,73 @@ class FamilyTreeView extends StatefulWidget {
 }
 
 class FamilyTreeViewState extends State<FamilyTreeView> {
+  final Graph graph = Graph();
   final Future<List<ChildToParent>> familyTree =
       FamilyService().getFamilyTree(ThePersonSingleton().user!);
   SugiyamaConfiguration builder = SugiyamaConfiguration();
+
+  List<Node> nodes = [];
 
   void addFamilyTree() {
     widget.setAddFamilyTree(null);
   }
 
+  Future<void> loadNodePositions(List<Node> nodes) async {
+    for (Node node in nodes) {
+      try {
+        Offset position = await NodePositionService().loadPosition(node);
+        node.x = position.dx;
+        node.y = position.dy;
+      } catch (e) {
+        //
+      }
+    }
+  }
+
+  void createEdges(List<Node> nodes, List<ChildToParent> data) {
+    for (Node node in nodes) {
+      try {
+        for (ChildToParent childToParent in data) {
+          if (node.key!.value.id == childToParent.child.id) {
+            Node possibleParentNode = nodes.firstWhere((element) =>
+                element.key!.value.id == childToParent.parent.id);
+
+            graph.addEdge(node, possibleParentNode);
+          }
+        }
+      } catch (e) {
+        //
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    // Save node positions when we exit that screen
+    saveNodePositions();
+    super.dispose();
+  }
+
+  Future<void> saveNodePositions() async {
+    for (Node node in nodes) {
+      await NodePositionService().savePosition(node);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      HeaderAddWithSecondAction(
+    return Column(
+      children: [
+        HeaderAddWithSecondAction(
           addAction: addFamilyTree,
           secondButtonAction: widget.goToChildToParentList,
           secondIcon: Icons.list,
-          title: "Family Tree"),
-      FutureBuilder(
+          title: "Family Tree",
+        ),
+        FutureBuilder(
           future: familyTree,
           builder: (context, snapshot) {
-            final Graph graph = Graph()..isTree = true;
-
             if (snapshot.hasData) {
-              List<Node> nodes = [];
               Map<int, Person> ids = {};
 
               for (ChildToParent childToParentNode in snapshot.data ?? []) {
@@ -57,25 +102,13 @@ class FamilyTreeViewState extends State<FamilyTreeView> {
                 }
               }
 
-              // Sometimes an edge added before node, so it does not show
               for (Node node in nodes) {
                 graph.addNode(node);
               }
 
-              for (Node node in nodes) {
-                try {
-                  for (ChildToParent childToParent in snapshot.data ?? []) {
-                    if (node.key!.value.id == childToParent.child.id) {
-                      Node possibleParentNode = nodes.firstWhere((element) =>
-                          element.key!.value.id == childToParent.parent.id);
+              loadNodePositions(nodes);
 
-                      graph.addEdge(node, possibleParentNode);
-                    }
-                  }
-                } catch (e) {
-                  //
-                }
-              }
+              createEdges(nodes, snapshot.data ?? []);
 
               builder.orientation = 1;
               builder.levelSeparation = 32;
@@ -84,37 +117,51 @@ class FamilyTreeViewState extends State<FamilyTreeView> {
               builder.bendPointShape = CurvedBendPointShape(curveLength: 5);
 
               return Expanded(
-                  child: InteractiveViewer(
-                      constrained: false,
-                      boundaryMargin: const EdgeInsets.all(64),
-                      minScale: 0.1,
-                      maxScale: 100.6,
-                      child: GraphView(
-                          graph: graph,
-                          algorithm: FruchtermanReingoldAlgorithm(
-                              attractionPercentage: 1,
-                              attractionRate: 1,
-                              repulsionPercentage: 0.1,
-                              repulsionRate: 0.1,
-                              iterations: 1),
-                          paint: Paint()
-                            ..color = Theme.of(context).primaryColorDark
-                            ..strokeWidth = 1
-                            ..style = PaintingStyle.stroke,
-                          builder: (Node node) {
-                            Person familyNode = node.key?.value;
-                            return Container(
-                                padding: const EdgeInsets.all(16),
-                                decoration: BoxDecoration(
-                                  color: Theme.of(context).primaryColor,
-                                  shape: BoxShape.circle,
-                                ),
-                                child: Text(
-                                    "${(familyNode.firstName.isNotEmpty) ? familyNode.firstName[0] : ""}${(familyNode.lastName.isNotEmpty) ? familyNode.lastName[0] : ""}"));
-                          })));
+                child: InteractiveViewer(
+                  constrained: false,
+                  boundaryMargin: const EdgeInsets.all(64),
+                  minScale: 0.1,
+                  maxScale: 100.6,
+                  onInteractionEnd: (details) async {
+                    // Save nodes with a tap of the screen
+                    for (Node node in nodes) {
+                      await NodePositionService().savePosition(node);
+                    }
+                  },
+                  child: GraphView(
+                    graph: graph,
+                    algorithm: FruchtermanReingoldAlgorithm(
+                      attractionPercentage: 1,
+                      attractionRate: 1,
+                      repulsionPercentage: 0.1,
+                      repulsionRate: 0.1,
+                      iterations: 1,
+                    ),
+                    paint: Paint()
+                      ..color = Theme.of(context).primaryColorDark
+                      ..strokeWidth = 1
+                      ..style = PaintingStyle.stroke,
+                    builder: (Node node) {
+                      Person familyNode = node.key?.value;
+                      return Container(
+                        padding: const EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          shape: BoxShape.circle,
+                        ),
+                        child: Text(
+                          "${(familyNode.firstName.isNotEmpty) ? familyNode.firstName[0] : ""}${(familyNode.lastName.isNotEmpty) ? familyNode.lastName[0] : ""}",
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              );
             }
             return Container();
-          })
-    ]);
+          },
+        ),
+      ],
+    );
   }
 }

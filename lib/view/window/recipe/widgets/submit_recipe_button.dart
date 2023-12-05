3730a4d7ad6648b48f137a159a614c616a7de6a5
Owen Bean <owenbean@Owens-MBP-98.lan>
2023-12-05 17:46:31 -0500

    Added error notification with snackbar

--- lib/view/window/recipe/widgets/submit_recipe_button.dart
+++ lib/view/window/recipe/widgets/submit_recipe_button.dart
@@ -3,15 +3,22 @@ import 'package:provider/provider.dart';
 import 'package:recipe_roots/domain/entire_recipe.dart';
 import 'package:recipe_roots/domain/entire_recipe_form.dart';
 import 'package:recipe_roots/service/recipe_service.dart';
+import 'package:recipe_roots/view/widget/snackbar_error.dart';
 
 class SubmitRecipeFormButton extends StatelessWidget {
   final Function goToRecipeViews;
 
   const SubmitRecipeFormButton({super.key, required this.goToRecipeViews});
 
-  saveRecipe(EntireRecipe recipe) {
+  saveRecipe(EntireRecipe recipe, BuildContext context) {
     if (recipe.recipe.id == null) {
-      RecipeService().addRecipe(recipe).then((value) => goToRecipeViews());
+      RecipeService()
+          .addRecipe(recipe)
+          .then((value) => goToRecipeViews())
+          .catchError((e) => {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(getErrorSnackbar(e.toString(), context))
+              });
     } else {
       RecipeService().editRecipe(recipe).then((value) => goToRecipeViews());
     }
@@ -22,7 +29,7 @@ class SubmitRecipeFormButton extends StatelessWidget {
     return Consumer<EntireRecipeForm>(builder: ((context, recipeForm, child) {
       return ElevatedButton(
         onPressed: () {
-          saveRecipe(recipeForm.getRecipe());
+          saveRecipe(recipeForm.getRecipe(), context);
         },
         style: Theme.of(context).elevatedButtonTheme.style,
         child: Padding(
